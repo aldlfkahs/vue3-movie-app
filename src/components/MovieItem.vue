f12bea2acf676094a28bad849347325ad66248f3
Seungwon Lee <aldlfkahs@gmail.com>
2023-11-19 00:19:13 +0900

    영화 정보 요청

---
+++ src/components/MovieItem.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="test">
+    {{ movie.imdbID }}
+  </div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    movie: {
+      type: Object,
+      default: () => ({})
+    }
+  }
+}
+</script>
